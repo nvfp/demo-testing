d46bedb1fa1f138d001d785d04e2095da8a1c334
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-17 05:24:11 -0700

    test 3022: working on Set up job; setting debugger for debugging

--- nics-compiler/workflows/setup-working-dir.sh
+++ nics-compiler/workflows/setup-working-dir.sh
@@ -1,12 +1,32 @@
 echo "::group::Set up NICS working directory"
 
+
+echo "DEBUG: pwd: '$(pwd)'"
+echo 'DEBUG: Running `ls`'
+ls
+echo "DEBUG: ls command completed"
+
+echo 'DEBUG: Running `cd ..`'
 cd ..
+
+echo "DEBUG: pwd: '$(pwd)'"
+echo 'DEBUG: Running `ls`'
+ls
+echo "DEBUG: ls command completed"
+
+echo 'DEBUG: Running `mkdir __nics_work_dir__`'
 mkdir __nics_work_dir__
+
+echo 'DEBUG: Running `ls`'
+ls
+echo "DEBUG: ls command completed"
+
+echo "DEBUG: Running 'cp $GH_REPO_NAME/$CONTAINER/ __nics_work_dir__/'"
 cp $GH_REPO_NAME/$CONTAINER/ __nics_work_dir__/
 
-echo "::debug::Foo"
-echo "---"
+echo 'DEBUG: Running `ls __nics_work_dir__`'
 ls __nics_work_dir__
-echo "---"
+echo "DEBUG: ls command completed"
+
 
 echo "::endgroup::"
