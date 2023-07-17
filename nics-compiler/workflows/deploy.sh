1accd17d915644871635eaffdc2b0e4f1de9894f
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-17 00:31:49 -0700

    added mock dir nics-compiler

---
+++ nics-compiler/workflows/deploy.sh
@@ -0,0 +1,6 @@
+echo "deploy.sh: '$foo'"
+
+# git config user.name "{author}"
+# git config user.email "{email}"
+# git commit -am "NICS rebuilds the docs — $(date +'%Y %b %e, %l:%M %p')"
+# git push
