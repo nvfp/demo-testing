4a690f6a371a2b8394d6ffc508d8827b88cda659
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-17 05:13:42 -0700

    test 3021: added debug.sh

--- nics-compiler/workflows/setup-working-dir.sh
+++ nics-compiler/workflows/setup-working-dir.sh
@@ -2,7 +2,7 @@ echo "::group::Set up NICS working directory"
 
 cd ..
 mkdir __nics_work_dir__
-cp $GH_REPO/$CONTAINER/ __nics_work_dir__/
+cp $GH_REPO_NAME/$CONTAINER/ __nics_work_dir__/
 
 echo "::debug::Foo"
 echo "---"
